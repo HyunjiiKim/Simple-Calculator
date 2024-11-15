428aeb4e0564b93c3fee1a4c7245cf07a8f0802f
hyunjiikim <hjkim96727@gmail.com>
2024-11-15 03:26:45 +0100

    Implement Component Life Cycle

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,38 @@ import Header from './components/Header/Header'
 import Viewer from './components/Viewer/Viewer'
 import Controller from './components/Controller/Controller'
 
-import { useState } from 'react'
+import { useRef,useState, useEffect } from 'react'
 
 import './App.css'
 
 function App(){
 
   const [count,setCount] = useState(0);
-  const handleSetCount= (value) => {
+  const [text,setText] = useState("");
+  const handleSetCount = (value) => {
     setCount(count+value);
   };
+  const handleChangeText = (e) =>{
+    setText(e.target.value);
+  };
+  
+  const didMountRef = useRef(false);
+
+  useEffect(()=>{
+    if(!didMountRef.current){
+      didMountRef.current=true;
+      return;
+    }else{
+      console.log("Component Updated");
+    }});
+
 
   return (
     <>
       <Header />
+      <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
       <Viewer count={count} />
       <Controller handleSetCount={handleSetCount}/>  
     </>
--- src/components/Controller/Controller.jsx
+++ src/components/Controller/Controller.jsx
@@ -1,24 +1,16 @@
 import './Controller.css';
 import React from "react";
 
+const elements=[+100,+10,+1,-1,-10,-100];
+
 const Controller = ({handleSetCount}) => {
     return(
         <div className='gridContainer'>
-            <button className='gridItem' onClick={()=>handleSetCount(+1)}>1</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+2)}>2</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+3)}>3</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+4)}>4</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+5)}>5</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+6)}>6</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+7)}>7</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+8)}>8</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+9)}>9</button>
-            <button className='gridItem'>+</button>
-            <button className='gridItem' onClick={()=>handleSetCount(+0)}>0</button>
-            <button className='gridItem'>-</button>
-            <button className='gridItem'>*</button>
-            <button className='gridItem'>/</button>
-            <button className='gridItem'>Clear</button>
+            {elements.map((value, index) => ( 
+                 <button 
+                 key={index}
+                 className='gridItem'
+                 onClick={()=>handleSetCount(+value)}>{value}</button>))}
         </div>
     );
 }
